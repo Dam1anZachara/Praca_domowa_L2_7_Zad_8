2bcd0c1b52454844809311ea185c7668e5ee91fc
dzachara <dzachara@ZACHARA-D>
2021-12-16 14:18:08 +0100

    Add project files.

---
+++ L2_7_Zad_8/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace L2_7_Zad_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program sprawdzi czy kandydat może ubiegać się o miejsce na studiach.");
+            Console.Write("Podaj wynik matury z matematyki: ");
+            byte.TryParse(Console.ReadLine(), out byte mathScore);
+
+            Console.Write("Podaj wynik matury z chemii: ");
+            byte.TryParse(Console.ReadLine(), out byte chemistryScore);
+
+            Console.Write("Podaj wynik matury z fizyki: ");
+            byte.TryParse(Console.ReadLine(), out byte physicsScore);
+
+            if ((mathScore > 70 && physicsScore > 55 && chemistryScore > 45) || (mathScore + physicsScore + chemistryScore > 180) || (mathScore + physicsScore > 150) || (mathScore + chemistryScore > 150))
+            {
+                Console.WriteLine("Kandydat dopuszczony do rekrutacji");
+            }
+            else
+            {
+                Console.WriteLine("Kandydat nie dopuszczony do rekrutacji");
+            }
+        }
+    }
+}
